6d750cd768c1d7fc44aaaeec24eae91d6956c40b
TSYW01 <129975299+TSYW01@users.noreply.github.com>
2024-01-20 22:11:29 -0800

    Update sshell.c
    trying phase4

--- sshell.c
+++ sshell.c
@@ -1,9 +1,11 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
+
 
 #define CMDLINE_MAX 512
 #define ARGS_MAX 32
@@ -12,6 +14,27 @@ struct Command {
     char *program;
     char *args[ARGS_MAX];
 };
+
+void output_redirect(char *cmdline) {
+    int fd;
+    char *meta_character = strchr(cmdline, '>');
+    // Parse the output file name from command line
+    if (meta_character) 
+    {
+        char *output_file = meta_character + 1;
+        while (*output_file == ' ') {
+            output_file++;
+        }
+
+        fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (fd == -1) {
+            perror("Error opening file");
+            exit(EXIT_FAILURE);
+        }
+        dup2(fd, STDOUT_FILENO);
+        close(fd);
+    }
+}
 
 void parse_command(char *cmdline, struct Command *command) {
     char *token;
@@ -61,6 +84,8 @@ int main(void) {
         if (nl)
             *nl = '\0';
 
+        output_redirect(cmd);
+
         /* Builtin commands */
         if (!strcmp(cmd, "exit")) { //exit
             fprintf(stderr, "Bye...\n");
@@ -89,7 +114,6 @@ int main(void) {
         else {
             /* Parse the command line */
             parse_command(cmd, &command);
-
             /* Fork a child process */
             pid = fork();
 
